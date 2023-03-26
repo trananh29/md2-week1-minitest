a813a3ca3fc1eec5b170e37eff1b799f4e01bd28
viyeumaden <66916286+viyeumaden@users.noreply.github.com>
2023-03-26 22:19:21 +0700

    xong b4

--- b4/src/main/java/Student.java
+++ b4/src/main/java/Student.java
@@ -25,17 +25,30 @@ public class Student {
 
     public static void main(String[] args) {
         Student newStuden1= new Student();
-        Scanner sc =new Scanner(System.in);
-        System.out.println("input Name");
-        newStuden1.setName(sc.nextLine());
-        System.out.println("input Age");
-        newStuden1.setAge(sc.nextInt());
-        System.out.println("input Class");
-        newStuden1.setClas(sc.nextLine());
-        System.out.println("the person is");
-
-        System.out.println(newStuden1.getName()+ newStuden1.getClas()+newStuden1.getAge());
+        newOne(newStuden1);
+//        Scanner sc =new Scanner(System.in);
+//        System.out.println("input Name");
+//        newStuden1.setName(sc.nextLine());
+//        System.out.println("input class");
+//        newStuden1.setClas(sc.nextLine());
+//        System.out.println("input Age");
+//        newStuden1.setAge(sc.nextInt());
+//        sc.close();
+//        System.out.println("the person is");
+//
+//       System.out.println(newStuden1.getName()+ newStuden1.getClas()+newStuden1.getAge());
     }
-
+public static Student newOne(Student newFresher) {
+    Scanner sc1 =new Scanner(System.in);
+    System.out.println("input Name");
+    newFresher.setName(sc1.nextLine());
+    System.out.println("input class");
+    newFresher.setClas(sc1.nextLine());
+    System.out.println("input Age");
+    newFresher.setAge(sc1.nextInt());
+    sc1.close();
+    System.out.println(newFresher.getName()+ newFresher.getClas()+newFresher.getAge());
+    return newFresher;
+}
 }
 
